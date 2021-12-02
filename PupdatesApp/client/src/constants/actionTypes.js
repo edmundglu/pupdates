341c155721da5a4ad7594262c1a94b34b9f793a4
Edmund Lu <52476608+edmundglu@users.noreply.github.com>
2021-12-02 12:30:34 -0800

    Delete PupdatesApp directory

--- PupdatesApp/client/src/constants/actionTypes.js
+++
@@ -1,6 +0,0 @@
-export const CREATE = 'CREATE';
-export const UPDATE = 'UPDATE';
-export const DELETE = 'DELETE';
-export const FETCH_ALL = 'FETCH_ALL';
-
-//use this because if you mispell something, it will show as a error
